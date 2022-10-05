c2ad453d951a81fe19d81e5a49bdd268e45d1d14
xmin12 <112725311+xmin12@users.noreply.github.com>
2022-10-05 16:14:00 +0200

    New

---
+++ main.c
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: jeyan
+ *
+ * Created on 05. Oktober 2022, 14:43
+ */
+
+#include <stdio.h>
+
+int main() {
+  char thema[] = "Noser Young";
+    
+  printf("Wortumkehrer\n");
+                for (int i = 10; i > -1; i--) 
+                {
+   
+                    
+                    
+                    printf("Buchstabe %d: %c\n", i, thema[i]);
+  }
+                return 0;
+}
